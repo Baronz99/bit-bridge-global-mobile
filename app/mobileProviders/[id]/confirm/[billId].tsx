8f3f3a8668390b64af9deee73b96e84d92318e80
morris menanya <emmiemenz@gmail.com>
2025-08-08 15:08:22 +0100

    bill style

--- app/mobileProviders/[id]/confirm/[billId].tsx
+++ app/mobileProviders/[id]/confirm/[billId].tsx
@@ -1,11 +1,11 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Linking } from 'react-native'
 import React, { useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import useNotification from '@/hooks/useNotification'
 import { useIsFocused } from '@react-navigation/native'
 import useFetch from '@/services/useFetch'
 import { useAuth } from '@/services/useAuth'
-import { getPurchaseOrder } from '@/api/billOrder'
+import { confirmBillPayment, getPurchaseOrder } from '@/api/billOrder'
 
 const confirm = () => {
    const {billId} = useLocalSearchParams()
@@ -21,6 +21,50 @@ const confirm = () => {
           }))
           const isFocused = useIsFocused()
             const [getstarted, setOpenStarted] = useState(false)
+
+
+            
+                const handleConfirmation = async (payment_method: string) => {
+                  // setTextInfo("Please wait while we process your payment")
+                  setLoader(true)
+                  // setNotification({
+                  //   error: false,
+                  //   message: "Recharge Successful",
+                  //   data: null
+                  // })
+            
+                  try {
+            
+                   const response = await  confirmBillPayment({queryId: billId, payment_method, token})
+                   setLoader(false)
+            
+                   if(payment_method === "card"){
+                    Linking.openURL(response.responseBody.checkoutUrl)
+            
+                   }
+            
+                   setNotification({
+                    error: false,
+                    message: response?.message || "Recharge Successful",
+                    data: null
+                  })
+            
+                  loadProfile(token)
+                   
+              
+                    
+                  } catch (error: any) {
+                    setLoader(false)
+                    setNotification({
+                      error: true,
+                      message: error.message || "something went wrong",
+                      data: null
+                    })
+                    
+                  }
+              
+            
+            }
           
      
             
@@ -30,7 +74,7 @@ const confirm = () => {
             onPress={() => setTextInfo("wallet")}
             >
             
-            <Text className=' font-semibold text-base text-gray-100'>Button for bill {textInfo}</Text>
+            <Text className=' font-semibold text-base text-gray-100'>Button for bill pay {textInfo}</Text>
             
         </TouchableOpacity>    
     </View>
