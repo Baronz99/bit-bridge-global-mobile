3a51b4e92ead06dc24b34ee64a66d0466caf0d11
morris menanya <emmiemenz@gmail.com>
2025-08-11 00:48:12 +0100

    remove padding

--- components/view-box/ViewBoxIcon.tsx
+++ components/view-box/ViewBoxIcon.tsx
@@ -15,7 +15,7 @@ import { Image, ImageProps, Text, TouchableOpacity, View } from "react-native"
       <Link href={link} asChild>      
         <TouchableOpacity className=' w-1/4 justify-center items-center'>
           <View className='w-14 h-14 bg-white/20 rounded-full justify-center items-center'>
-            <Image source={icon}  className='w-6 h-6 rounded-full p-4' />
+            <Image source={icon}  className='w-6 h-6 rounded-full' />
           </View>
             <Text className='text-white'>{label}</Text>
 
